337dc72f764e3fb08d1b276b8c83a587a3d16668
Catiel <oliver.alandiaa@gmail.com>
2025-10-15 08:37:47 -0400

    ,

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 const inter = Inter({ subsets: ["latin"] });
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Aprende Code",
